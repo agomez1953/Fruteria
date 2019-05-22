2ff9773798440bf0992c7228a062e006eb1bd14e
alumno <2000afgt@gmail.com>
2019-05-21 19:26:33 -0500

    s

--- Fruta.py
+++ Fruta.py
@@ -1,13 +1,24 @@
 class Fruta:
-    sabor : str
-    pelada : str
-    cantidad : str
+
+    def __init__(self,sabor,pelada,cantidad):
+        self.sabor = sabor
+        self.pelada = pelada
+        self.cantidad = cantidad
 
     def pelar(self):
-        return True
+        if self.pelada:
+            self.pelada = True
+
+        elif self.pelada:
+            self.pelada = False
+
+        return self.pelada
 
     def cortar(self, cantidad: int):
-        return 2
+        if self.pelada and cantidad > 1:
+            cantidad -= 1
+            return cantidad
+
 
     def licuar(self, cantidad: int):
         return 2.0
--- Frutiadero.py
+++ Frutiadero.py
@@ -3,3 +3,4 @@ class Frutiadero:
     inventario : str
 
     def preparar(self,tipo : str, ingredientes):
+        return 0
